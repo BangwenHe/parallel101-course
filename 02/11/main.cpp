dee717db560475b2be7cffa9e23f0925120c0ac4
Hebangwen <bangwhe@qq.com>
2022-08-21 11:15:07 +0800

    feat: add experiment of move assginment operator

--- 02/11/main.cpp
+++ 02/11/main.cpp
@@ -35,11 +35,12 @@ struct Vector {
         other.m_data = nullptr;
     }
 
-    Vector &operator=(Vector &&other) {
-        this->~Vector();
-        new (this) Vector(std::move(other));
-        return *this;
-    }
+    // Vector &operator=(Vector &&other) {
+    //     this->~Vector();
+    //     new (this) Vector(std::move(other));
+    //     return *this;
+    // }
+    Vector &operator=(Vector &&) = delete;
 
     size_t size() {
         return m_size;
@@ -57,9 +58,17 @@ struct Vector {
 
 int main() {
     Vector v1(32);
-    Vector v2(64);
+    v1[0] = 0;
+    v1[1] = 1;
+    // Vector v2(64);
+    std::cout << v1[0] << " " << v1[1] << std::endl;
 
-    v2 = std::move(v1);
+    // v2 = std::move(v1);
+    Vector v2 = v1;
+    v2[0] = 2;
+    std::cout << v2[0] << " " << v2[1] << std::endl;
+
+    // v2 = std::move(v1);  // 编译无法通过, 调用了移动赋值函数
 
     return 0;
 }
